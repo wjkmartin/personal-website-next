1671aeec03c990a6bf0e6e149f951db0a79da2de
William Martin <wjkmartin@gmail.com>
2021-10-19 08:58:43 -0700

    Complete header styling

--- src/common/HeaderModal.tsx
+++ src/common/HeaderModal.tsx
@@ -1,13 +1,38 @@
-import React, {useState} from 'react';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import styles from "../styles/Header.module.css"
+import styles from '../styles/Header.module.css';
+import HeaderLink from './HeaderLink';
 
 const HeaderModal = () => {
-    const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
-  return <FontAwesomeIcon onClick={() => {setIsSelected(!isSelected)}} icon={faBars} className={isSelected ? `${styles.Header__modal} ${styles.Header__link_currentRoute}` : `${styles.Header__modal} ${styles.Header__link}`} />
+  const dropdown = (
+    <div className={styles.Header__modal_dropDown}>
+      <HeaderLink href="/about" text="ABOUT"/>
+      <HeaderLink href="/blog" text="BLOG" />
+      <HeaderLink href="/portfolio" text="PORTFOLIO" />
+      <HeaderLink href="/contact" text="CONTACT" />
+    </div>
+  );
+
+  return (
+    <>
+    <FontAwesomeIcon
+      onClick={() => {
+        setIsSelected(!isSelected);
+      }}
+      icon={faBars}
+      className={
+        isSelected
+          ? `${styles.Header__modal} ${styles.Header__link_currentRoute}`
+          : `${styles.Header__modal} ${styles.Header__link}`
+      }
+    />
+    {(isSelected ? dropdown : '')}
+  </>
+  )
 };
 
 export default HeaderModal;
