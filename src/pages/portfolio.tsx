755493f6f76172633acb1891a1e9e3a825095883
William Martin <wjkmartin@gmail.com>
2021-10-19 08:19:42 -0700

    Fix Header style for modal ; Add linkbar Portfolio

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,14 +1,19 @@
 import _Head from '../common/_Head';
-import styles from '../styles/Home.module.css';
+import commonStyles from '../styles/Home.module.css';
+import styles from '../styles/Portfolio.module.css';
 import Header from '../common/Header';
 
 export default function Portfolio() {
   return (
-    <div className={styles.container}>
+    <div className={commonStyles.container}>
       <_Head />
       <Header />
-
-      <main className={styles.main}>PORTFOLIO</main>
+      <main className={commonStyles.main}>
+        <div className={styles.Portfolio__linkBar}>
+          <a className={styles.Portfolio__linkBar_link} href="https://github.com/wjkmartin"><span style={{filter: 'grayscale(.95)'}}>👨‍💻</span> Github</a>
+          <a className={styles.Portfolio__linkBar_link} href="https://www.hackerrank.com/wjkmartin"><span style={{filter: 'grayscale(.95)'}}>🥇</span> HackerRank</a>
+        </div>
+      </main>
 
       <footer className={styles.footer}></footer>
     </div>
