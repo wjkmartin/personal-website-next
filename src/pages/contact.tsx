eb04130390d74d681f131ae16444c14c80229be3
William Martin <wjkmartin@gmail.com>
2021-10-21 07:45:26 -0700

    Add basic contact form API route

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,41 +1,43 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react';
 import Header from '../common/Header';
 import _Head from '../common/_Head';
 import commonStyles from '../styles/commonStyles.module.css';
 import styles from '../styles/Contact.module.css';
 
-import nodemailer from 'nodemailer'
-
 export default function Contact() {
-  async function sendEmail() {
-    let testAccount = await nodemailer.createTestAccount();
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-      },
-    });
-  
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-  
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
+  const [nameInput, setNameInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [messageInput, setMessageInput] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
+  async function sendEmail(e) {
+    e.preventDefault();
+
+    const data = {
+      nameInput,
+      emailInput,
+      messageInput,
+    };
+
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log('Response received');
+      if (res.status === 200) {
+        console.log('Response succeeded!');
+        setSubmitted(true);
+        setNameInput('');
+        setEmailInput('');
+        setMessageInput('');
+      }
+    });
+  }
 
   return (
     <div className={commonStyles.container}>
@@ -48,26 +50,47 @@ export default function Contact() {
             Please fill out the form below and I'll get back to you as soon as I
             can.
           </p>
-          <form
-            className={styles.Contact__form}
-            acceptCharset="UTF-8"
-          >
+          <form className={styles.Contact__form} acceptCharset="UTF-8">
             <div className={styles.Contact__form_row}>
               <div className={styles.Contact__smallField}>
                 <label className={styles.Contact__label}>NAME</label>
-                <input className={styles.Contact__smallField_input}></input>
+                <input
+                  onInput={(e) =>
+                    setNameInput((e.target as HTMLInputElement).value)
+                  }
+                  className={styles.Contact__smallField_input}
+                ></input>
               </div>
               <div className={styles.Contact__smallField}>
                 <label className={styles.Contact__label}>EMAIL</label>
-                <input className={styles.Contact__smallField_input}></input>
+                <input
+                  onInput={(e) =>
+                    setEmailInput((e.target as HTMLInputElement).value)
+                  }
+                  className={styles.Contact__smallField_input}
+                ></input>
               </div>
             </div>
-            
-              <div className={styles.Contact_largeField}>
-                <label className={styles.Contact__label}>MESSAGE</label>
-                <textarea className={styles.Contact__largeField_input}></textarea>
-              </div>
-            <button onClick={() => {sendEmail()}} className={styles.Contact_button}>SEND IT!</button>
+
+            <div className={styles.Contact_largeField}>
+              <label className={styles.Contact__label}>MESSAGE</label>
+              <textarea
+                onInput={(e) =>
+                  setMessageInput((e.target as HTMLInputElement).value)
+                }
+                id="messageInput"
+                className={styles.Contact__largeField_input}
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              onClick={(e) => {
+                sendEmail(e);
+              }}
+              className={styles.Contact_button}
+            >
+              SEND IT!
+            </button>
           </form>
         </div>
       </main>
