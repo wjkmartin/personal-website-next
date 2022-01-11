b6dbafd2761e3bb8cadce1430f41a2733636a48d
William Martin <wjkmartin@gmail.com>
2022-01-11 09:53:32 -0800

    fix: local import socials images

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './Socials.module.css';
 
+import twitterIcon from '../../public/twitter.png';
+import linkedinIcon from '../../public/linkedin.png';
+import githubIcon from '../../public/github.png';
+
 const Socials = () => {
   return (
     <div className={styles.Socials}>
@@ -11,9 +15,7 @@ const Socials = () => {
         href="https://twitter.com/willjmartin"
       >
         <Image
-          width="50px"
-          height="50px"
-          src="/twitter.png"
+          src={twitterIcon}
           alt="twitter icon"
         ></Image>
       </a>
@@ -22,18 +24,14 @@ const Socials = () => {
         href="https://www.linkedin.com/in/will-martin-a1890b184/"
       >
         <Image
-          width="50px"
-          height="50px"
-          src="/linkedin.png"
+          src={linkedinIcon}
           alt="linkedin icon"
         ></Image>
       </a>
 
       <a className={styles.Socials__item} href="https://github.com/wjkmartin">
         <Image
-          width="50px"
-          height="50px"
-          src="/github.png"
+          src={githubIcon}
           alt="github icon"
         ></Image>
       </a>
