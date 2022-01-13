610906dae2b9806b690fc42955d1301b3d828555
William Martin <wjkmartin@gmail.com>
2022-01-13 12:30:47 -0800

    fix: trendy loading bar

--- src/common/HeaderLink.tsx
+++ src/common/HeaderLink.tsx
@@ -7,7 +7,8 @@ const HeaderLink = (props) => {
     props.currentRoute === props.href
       ? styles.Header__link_currentRoute
       : styles.Header__link;
-  return (
+
+  const home = (
     <a
       href={props.href}
       className={`${style} ${
@@ -17,6 +18,19 @@ const HeaderLink = (props) => {
       {props.text}
     </a>
   );
+  if (props.href === '/') {
+    return home;
+  } else return (
+      <Link passHref href={props.href}>
+        <a
+          className={`${style} ${
+            props.currentRoute === '/' ? styles.Header__link_homepageOnly : ''
+          } ${props.text === 'WILL MARTIN' ? styles.Header__myName : ''}`}
+        >
+          {props.text}
+        </a>
+      </Link>
+    );
 };
 
 export default HeaderLink;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }) {
 
   return (
+    <>
+      <NextNProgress color='#F4A261'/>
       <Component  {...pageProps} />
+    </>
   )
 }
 
